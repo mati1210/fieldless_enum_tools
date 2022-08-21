62979fec9dbe720f58f2b3c256bbac15dab527cb
matheuz1210 <matheuz1210@gmail.com>
2022-08-21 00:49:09 -0300

    add docs for Not, make other docs better

--- src/lib.rs
+++ src/lib.rs
@@ -5,26 +5,80 @@ extern crate alloc;
 /// Adds an function `all` to enum, returning an array with all variants of the enum
 ///
 /// ```rust
-/// # use fieldless_enum_tools::All;
+/// use fieldless_enum_tools::All;
+///
 /// #[derive(All, Debug, PartialEq, Eq)]
 /// enum CoolEnum {
-///   CoolVariantOne,
-///   CoolVariantTwo
+///     CoolVariantOne,
+///     CoolVariantTwo
 /// }
 ///
-/// fn main() {
-///   assert_eq!{
+/// assert_eq!(
 ///     CoolEnum::all(),
 ///     [CoolEnum::CoolVariantOne, CoolEnum::CoolVariantTwo]
-///   }
-/// }
+/// );
 /// ```
 ///
 /// # Attributes
-/// `#[all_doc = r#"..."#)]` => sets the doc of the function. default being "Returns an array of all elements on this enum."
+///
+/// ## Outer attributes
+///
+/// `#[all_doc = r#"..."#)]`
+///
+/// Sets the doc of the function, with the default being "Returns an array of all elements on this enum."
 pub use fieldless_enum_tools_impl::All;
 
-/// TODO
+/// Implements [`Not`] for enum.
+///
+/// ```rust
+/// use fieldless_enum_tools::Not;
+///
+/// // works without any attributes on enum with up to two variants
+/// #[derive(Not, Debug, PartialEq, Eq)]
+/// enum One {
+///     One
+/// }
+///
+/// assert_eq!(!One::One, One::One);
+///
+/// #[derive(Not, Debug, PartialEq, Eq)]
+/// enum More {
+///     One,
+///     Two
+/// }
+///
+/// assert_eq!(!More::One, More::Two);
+/// assert_eq!(!More::Two, More::One);
+///
+/// // requires attributes after
+/// #[derive(Not, Debug, PartialEq, Eq)]
+/// enum Multiple {
+///     #[not(OppositeOfOne)]
+///     One,
+///     #[not(OppositeOfTwo)]
+///     Two,
+///     #[not(One)]
+///     OppositeOfOne,
+///     #[not(Two)]
+///     OppositeOfTwo,
+///     #[not(Trap)]
+///     Trap
+/// }
+///
+/// assert_eq!(!Multiple::One, Multiple::OppositeOfOne);
+/// assert_eq!(!Multiple::OppositeOfTwo, Multiple::Two);
+/// assert_eq!(!Multiple::Trap, Multiple::Trap);
+/// ```
+///
+/// # Attributes
+///
+/// ## Variant attributes
+///
+/// `#[not(...)]`
+///
+/// Specifies which variant this variant will return when [`Not`]'ed
+///
+/// [`Not`]: `core::ops::Not`
 pub use fieldless_enum_tools_impl::Not;
 
 /// Implements
@@ -34,8 +88,9 @@ pub use fieldless_enum_tools_impl::Not;
 /// [`TryFrom<String>`][^alloc],
 /// [`Serialize`][^serde] and [`Deserialize`][^serde] for enum.
 ///
-///```
-/// # use fieldless_enum_tools::FromToStr;
+///```rust
+/// use fieldless_enum_tools::FromToStr;
+///
 /// #[derive(FromToStr, Debug, PartialEq, Eq, Clone, Copy)]
 /// #[fromtostr(format(style = "delimited", separator = "😎"))]
 /// enum CoolEnum {
@@ -45,26 +100,26 @@ pub use fieldless_enum_tools_impl::Not;
 ///     CoolVariantTwo
 /// }
 ///
-/// fn main() {
-///     let cool = CoolEnum::CoolVariantOne;
 ///
-///     assert_eq!(cool.as_ref(), "Cool😎Variant😎One");
-///     assert_eq!("cool_variant_one".parse(), Ok(cool));
-///     assert_eq!("Cool😎Variant😎One".parse(), Ok(cool));
+/// let cool = CoolEnum::CoolVariantOne;
 ///
-///     assert_eq!("uncool variant :(".parse::<CoolEnum>(), Err(()));
+/// assert_eq!(cool.as_ref(), "Cool😎Variant😎One");
+/// assert_eq!("cool_variant_one".parse(), Ok(cool));
+/// assert_eq!("Cool😎Variant😎One".parse(), Ok(cool));
 ///
-///     let cool = CoolEnum::CoolVariantTwo;
+/// let cool = CoolEnum::CoolVariantTwo;
+/// assert_eq!("Very😎Cool😎Variant😎Two".parse(), Ok(cool));
 ///
-///     assert_eq!("Cool😎Variant😎Two".parse::<CoolEnum>(), Err(()));
-///     assert_eq!("Very😎Cool😎Variant😎Two".parse(), Ok(cool));
-/// }
+/// assert_eq!("uncool variant :(".parse::<CoolEnum>(), Err(()));
+/// // errors because we renamed it to Very😎Cool😎Variant😎Two
+/// assert_eq!("Cool😎Variant😎Two".parse::<CoolEnum>(), Err(()));
 ///```
 ///
 /// # Attributes
-/// ## Outer
+/// ## Outer attributes
 ///
 /// `#[fromtostr(skip(...*))]`
+///
 /// Skips implementing specified traits
 ///
 ///   **Possible Values**
@@ -82,9 +137,23 @@ pub use fieldless_enum_tools_impl::Not;
 /// ---
 ///
 /// `#[fromtostr(format(style = "...", separator = "..."?))]`
-/// Format variants using specified style
 ///
-/// **Possible Values**
+/// Format variants using specified [style](Self#possible-styles)
+///
+///
+/// ## Variant attributes
+///
+/// `#[fromtostr(aliases("..."*))]`
+///
+/// Specifies one (or more aliases) for this variant
+///
+/// ---
+///
+/// `#[fromtostr(rename("..."))]` or `#[fromtostr(rename(style = "...", separator = "..."?))]`
+///
+/// Renames this variant with specified string or specified [format style](Self#possible-styles)
+///
+/// # Possible Styles
 ///
 /// >| Style Name        | Description                                           | Example               | Note                                                    |
 /// >|-------------------|-------------------------------------------------------|-----------------------|---------------------------------------------------------|
@@ -103,18 +172,6 @@ pub use fieldless_enum_tools_impl::Not;
 /// >| `delimited`       | delimits every word with separator                    | `Two{separator}Words` | needs to specify a separator value                      |
 /// >| `delimitedlower`  | delimits every word with separator, then to lowercase | `two{separator}words` | needs to specify a separator value                      |
 /// >| `DELIMITEDUPPER`  | delimits every word with separator, then to uppercase | `TWO{SEPARATOR}WORDS` | needs to specify a separator value                      |
-///
-/// ## Inner
-///
-/// `#[fromtostr(aliases("..."*))]`
-///
-/// Specifies one (or more aliases) for this variant
-///
-/// ---
-///
-/// `#[fromtostr(rename("..."))]` or `#[fromtostr(rename(style = "...", separator = "..."?))]`
-///
-/// Renames this variant with specified string or specified format style
 
 ///
 /// [^alloc]: if crate feature `std` or `alloc` avaliable.
@@ -128,7 +185,7 @@ pub use fieldless_enum_tools_impl::Not;
 /// [`Deserialize`]: https://serde.rs/
 pub use fieldless_enum_tools_impl::FromToStr;
 
-#[doc(hidden)]
+#[cfg(not(doc))]
 pub mod __internal {
     #[cfg(all(feature = "alloc", not(feature = "std")))]
     pub use alloc::string::String;
@@ -140,42 +197,36 @@ pub mod __internal {
 
     #[cfg(not(feature = "serde"))]
     #[macro_export]
-    #[doc(hidden)]
     macro_rules! if_serde_enabled {
         ($($t:tt)*) => {};
     }
 
     #[cfg(feature = "serde")]
     #[macro_export]
-    #[doc(hidden)]
     macro_rules! if_serde_enabled {
         ($($t:tt)*) => { $($t)* };
     }
 
     #[cfg(not(any(feature = "alloc", feature = "std")))]
     #[macro_export]
-    #[doc(hidden)]
     macro_rules! if_alloc_enabled {
         ($($t:tt)*) => {};
     }
 
     #[cfg(any(feature = "alloc", feature = "std"))]
     #[macro_export]
-    #[doc(hidden)]
     macro_rules! if_alloc_enabled {
         ($($t:tt)*) => { $($t)* };
     }
 
     #[cfg(not(feature = "std"))]
     #[macro_export]
-    #[doc(hidden)]
     macro_rules! if_std_enabled {
         ($($t:tt)*) => {};
     }
 
     #[cfg(feature = "std")]
     #[macro_export]
-    #[doc(hidden)]
     macro_rules! if_std_enabled {
         ($($t:tt)*) => { $($t)* };
     }
